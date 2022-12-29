1aae37d0564224e1208289c216c2a9e9def3dbcb
wilberquito <wbq.software@gmail.com>
2022-12-29 10:12:15 +0100

    Function to handle multiple images in asyncron way

--- Code/App/main.py
+++ Code/App/main.py
@@ -1,7 +1,8 @@
 import os
 import shutil
 from typing import Union
-from fastapi import FastAPI, File, UploadFile, HTTPException
+import uuid
+from fastapi import FastAPI, File, UploadFile, HTTPException, Request, BackgroundTasks 
 
 app = FastAPI()
 
@@ -22,7 +23,7 @@ async def perform_predict(file: UploadFile = File(...),):
     temp_file = _save_file_to_disk(file, path="temp", save_as="latest")
     return {
         'filename': file.filename, 
-        'tempFilename': temp_file 
+        'file_id': temp_file
     }
     
 def _save_file_to_disk(file: UploadFile = File(...), path=".", save_as="default"):
@@ -34,7 +35,28 @@ def _save_file_to_disk(file: UploadFile = File(...), path=".", save_as="default"
         shutil.copyfileobj(file.file, f)
     return temp_file
 
-@app.get("/ml-models")
+@app.post("/predict_packet")
+async def perform_predict_packet(request: Request, bg_tasks: BackgroundTasks):
+    """
+    It avoids users to await to the prediction of every image. Instead, once the
+    images are downloaded I send the users the uuid of the folder where those images where saved.
+
+    docs: https://fastapi.tiangolo.com/tutorial/background-tasks/?h=background
+    """
+    images = await request.form()
+    folder_name = str(uuid.uuid4())
+    os.mkdir(folder_name)
+
+    for image in images.values():
+        _ = _save_file_to_disk(image, path=folder_name, save_as=image.filename)
+
+    bg_tasks.add_task(read_images_from_disk, folder_name)
+    return {
+        "task_id": folder_name,
+        "num_files": len(images)
+    }
+    
+@app.get("/ml_models")
 async def get_models():
     allowed_extensions = ('.h5', '.txt')
     models = list_files('models', allowed_extensions)
@@ -49,3 +71,7 @@ async def load_model(filename = 'h5.txt'):
     with open(filename, 'r') as f:
         content = f.read()
     return content
+
+async def read_images_from_disk(folder_name):
+    print(folder_name)
+    pass
