ba5fdb97cd7a69fc7d00296abe5884609d40a0ef
wilberquito <wbq.software@gmail.com>
2023-02-21 13:31:07 +0100

    feat: :sparkles: Add image downloader script

---
+++ Code/NN/download.sh
@@ -0,0 +1,10 @@
+mkdir ./data
+cd ./data
+for input_size in 512 768 1024
+do
+  kaggle datasets download -d cdeotte/jpeg-isic2019-${input_size}x${input_size}
+  kaggle datasets download -d cdeotte/jpeg-melanoma-${input_size}x${input_size}
+  unzip -q jpeg-melanoma-${input_size}x${input_size}.zip -d jpeg-melanoma-${input_size}x${input_size}
+  unzip -q jpeg-isic2019-${input_size}x${input_size}.zip -d jpeg-isic2019-${input_size}x${input_size}
+  rm jpeg-melanoma-${input_size}x${input_size}.zip jpeg-isic2019-${input_size}x${input_size}.zip
+done
