1cc1f3564f9cb35c151e95045991885cf5ca59ae
wilberquito <wbq.software@gmail.com>
2023-02-23 13:11:19 +0100

    fix: every time user loads images it cleans the images buffer

--- Code/UI/conv-ui/src/Loader.svelte
+++ Code/UI/conv-ui/src/Loader.svelte
@@ -4,6 +4,8 @@
   let images = [];
 
   function handleFiles(event) {
+
+    images = []
     const files = event.target.files;
 
     for (let i = 0; i < files.length; i++) {
@@ -45,6 +47,41 @@
         object-fit: cover;
         border-radius: 0.25rem;
       }
+
+      .file-input-wrapper {
+        position: relative;
+        display: inline-block;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+      }
+
+      .file-input-wrapper input[type="file"] {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+        cursor: pointer;
+      }
+
+      .file-input-button {
+        display: inline-block;
+        padding: 0.5rem 1rem;
+        font-size: 1rem;
+        font-weight: 500;
+        line-height: 1.5;
+        color: #fff;
+        background-color: #007bff;
+        border-radius: 0.25rem;
+        transition: background-color 0.15s ease-in-out, border-color 0.15s ease-in-out,
+          box-shadow 0.15s ease-in-out;
+        cursor: pointer;
+      }
+
+      .file-input-button:hover {
+        background-color: #0069d9;
+      }
     `;
 
     const styleElement = document.createElement('style');
@@ -53,10 +90,13 @@
   });
 </script>
 
-<input type="file" multiple on:change="{handleFiles}">
+<div class="file-input-wrapper">
+  <button class="file-input-button">Select Images</button>
+  <input type="file" multiple on:change="{handleFiles}">
+</div>
+
 <div class="image-container">
   {#each images as image}
     <img src="{image}" alt="User uploaded image">
   {/each}
 </div>
-
