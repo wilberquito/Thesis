90e8b4d074909d6d4c23a0b24a7db0bf179c9b03
wilberquito <wbq.software@gmail.com>
2023-02-24 13:08:43 +0100

    Updates layout to handle images

--- Code/UI/conv-ui/src/Loader.svelte
+++ Code/UI/conv-ui/src/Loader.svelte
@@ -1,11 +1,10 @@
 <script>
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
   let images = [];
 
   function handleFiles(event) {
-
-    images = []
+    images = [];
     const files = event.target.files;
 
     for (let i = 0; i < files.length; i++) {
@@ -18,85 +17,107 @@
       reader.readAsDataURL(files[i]);
     }
   }
-
-  onMount(() => {
-    const imageContainer = document.querySelector('.image-container');
-    const imageWidth = imageContainer.clientWidth / 3;
-    const numImages = images.length;
-
-    const style = `
-      .image-container {
-        display: flex;
-        flex-wrap: nowrap;
-        justify-content: center;
-        align-items: center;
-        padding: 1rem;
-        gap: 1rem;
-        overflow-x: auto;
-        scrollbar-width: none;
-        -ms-overflow-style: none;
-      }
-
-      .image-container::-webkit-scrollbar {
-        display: none;
-      }
-
-      .image-container img {
-        width: ${imageWidth}px;
-        height: ${imageWidth}px;
-        object-fit: cover;
-        border-radius: 0.25rem;
-      }
-
-      .file-input-wrapper {
-        position: relative;
-        display: inline-block;
-        margin-top: 1rem;
-        margin-bottom: 1rem;
-      }
-
-      .file-input-wrapper input[type="file"] {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        opacity: 0;
-        cursor: pointer;
-      }
-
-      .file-input-button {
-        display: inline-block;
-        padding: 0.5rem 1rem;
-        font-size: 1rem;
-        font-weight: 500;
-        line-height: 1.5;
-        color: #fff;
-        background-color: #007bff;
-        border-radius: 0.25rem;
-        transition: background-color 0.15s ease-in-out, border-color 0.15s ease-in-out,
-          box-shadow 0.15s ease-in-out;
-        cursor: pointer;
-      }
-
-      .file-input-button:hover {
-        background-color: #0069d9;
-      }
-    `;
-
-    const styleElement = document.createElement('style');
-    styleElement.innerHTML = style;
-    document.head.appendChild(styleElement);
-  });
 </script>
 
 <div class="file-input-wrapper">
   <button class="file-input-button">Select Images</button>
-  <input type="file" multiple on:change="{handleFiles}">
+  <input type="file" multiple on:change={handleFiles} />
 </div>
 
-<div class="image-container">
-  {#each images as image}
-    <img src="{image}" alt="User uploaded image">
-  {/each}
+<div class="container">
+  <div class="img-grid">
+    {#each images as image}
+      <div class="img-container">
+        <img src={image} alt="User uploaded image" />
+      </div>
+    {/each}
+  </div>
 </div>
+
+<style>
+
+  .container {
+    margin: auto;
+  }
+
+  .img-container {
+    width: 100%;
+    height: 100%;
+  }
+
+  /* mak images fill their container*/
+  img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+
+  img:hover {
+    opacity: 0.5;
+    cursor: pointer;
+  }
+
+  /* CSS Grid*/
+  .img-grid {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 5px;
+  }
+
+  /* Media Query for changing grid on bigger screens*/
+
+  /* Bigger than Phones(tablet) */
+  @media only screen and (min-width: 768px) {
+    .img-grid {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  /* Bigger than Phones(laptop / desktop) */
+  @media only screen and (min-width: 992px) {
+    .img-grid {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+
+  /* Bigger than Phones(laptop / desktop) */
+  @media only screen and (min-width: 1200px) {
+    .img-grid {
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+
+  .file-input-wrapper {
+    position: relative;
+    display: inline-block;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+
+  .file-input-wrapper input[type="file"] {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    cursor: pointer;
+  }
+  .file-input-button {
+    display: inline-block;
+    padding: 0.5rem 1rem;
+    font-size: 1rem;
+    font-weight: 500;
+    line-height: 1.5;
+    color: #fff;
+    background-color: #007bff;
+    border-radius: 0.25rem;
+    transition: background-color 0.15s ease-in-out,
+      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    cursor: pointer;
+  }
+
+  .file-input-button:hover {
+    background-color: #0069d9;
+  }
+</style>
