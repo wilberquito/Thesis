4eb6f55aff7794d187758df407143822e24c805d
wilberquito <wbq.software@gmail.com>
2023-03-10 11:49:32 +0100

    changes deprecated modules to supported ones

--- Code/nn/modular/dataset.py
+++ Code/nn/modular/dataset.py
@@ -10,6 +10,7 @@ from torch.utils.data import Dataset
 
 
 class MelanomaDataset(Dataset):
+    """Definition of the dataset for the melanoma problem"""
 
     def __init__(self, csv, mode, transform=None):
         self.csv = csv.reset_index(drop=True)
@@ -128,7 +129,6 @@ def get_df(out_dim: int, data_dir: str, data_folder: str):
 
 
 def get_transforms(image_size):
-
     """
     Returns a pair of transformers.
     The first transformer applies image augmentation of different kinds and resize the image.
@@ -140,25 +140,33 @@ def get_transforms(image_size):
         A.VerticalFlip(p=0.5),
         A.HorizontalFlip(p=0.5),
         A.RandomBrightness(limit=0.2, p=0.75),
-        A.RandomContrast(limit=0.2, p=0.75),
+        A.RandomBrightnessContrast(limit=0.2, p=0.75),
         A.OneOf([
             A.MotionBlur(blur_limit=5),
             A.MedianBlur(blur_limit=5),
             A.GaussianBlur(blur_limit=5),
             A.GaussNoise(var_limit=(5.0, 30.0)),
         ], p=0.7),
-
         A.OneOf([
             A.OpticalDistortion(distort_limit=1.0),
             A.GridDistortion(num_steps=5, distort_limit=1.),
             A.ElasticTransform(alpha=3),
         ], p=0.7),
-
         A.CLAHE(clip_limit=4.0, p=0.7),
-        A.HueSaturationValue(hue_shift_limit=10, sat_shift_limit=20, val_shift_limit=10, p=0.5),
-        A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=15, border_mode=0, p=0.85),
+        A.HueSaturationValue(hue_shift_limit=10,
+                             sat_shift_limit=20,
+                             val_shift_limit=10,
+                             p=0.5),
+        A.ShiftScaleRotate(shift_limit=0.1,
+                           scale_limit=0.1,
+                           rotate_limit=15,
+                           border_mode=0,
+                           p=0.85),
         A.Resize(image_size, image_size),
-        A.Cutout(max_h_size=int(image_size * 0.375), max_w_size=int(image_size * 0.375), num_holes=1, p=0.7),
+        A.CourseDropout(num_holes=1,
+                        max_h_size=int(image_size * 0.375),
+                        max_w_size=int(image_size * 0.375),
+                        p=0.7),
         A.Normalize()
     ])
 
@@ -172,6 +180,7 @@ def get_transforms(image_size):
 
 def plot_dataset_samples(dataset: MelanomaDataset, rows=3, cols=3, seed=None):
     """Plots nrows*ncols random images"""
+
     if seed is not None:
         torch.manual_seed(seed)
     fig = plt.figure(figsize=(9, 9))
