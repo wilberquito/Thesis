3fe2db5e8f73b89c00f52b4b5a4badc5a0d376c5
wilberquito <wbq.software@gmail.com>
2023-03-14 14:45:20 +0100

    unneded install file from remote

--- Code/api/main.py
+++ Code/api/main.py
@@ -1,16 +1,16 @@
 import logging
-import subprocess
+# import subprocess
 
 import uvicorn
 
 import modular.vision as vision
 
 
-def pull_shared_code():
-    subprocess.call(['sh', 'pull_nn_code.sh'])
+# def pull_shared_code():
+#     subprocess.call(['sh', 'pull_nn_code.sh'])
 
 def main():
-    pull_shared_code()
+#   pull_shared_code()
     logging.basicConfig(filename='myapp.log', level=logging.INFO, datefmt='%m/%d/%Y %I:%M:%S %p')
     vision.run()
     uvicorn.run("api:app", host="0.0.0.0", port=8081, reload=True)
