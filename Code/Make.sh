c9929c2ed9ba0bbc9bbe2c9d909f54fb8e1ec726
wilberquito <wbq.software@gmail.com>
2023-05-02 23:42:13 +0200

    Make file to deploy project

--- Code/Make.sh
+++ Code/Make.sh
@@ -1,7 +1,30 @@
+#!bin/sh
+
+SOURCE_CODE_PATH=~/repos/thesis
+
 # Make the api image
+VICOROBOT_PKG_PATH=$SOURCE_CODE_PATH/vicorobot.pkg
+API_PATH=$SOURCE_CODE_PATH/api
+API_IMG_NAME=melanoma_api
+
+# Adds vicorobot source code to work with its
+# trained models
+cp -rf $VICOROBOT_PKG_PATH $API_PATH && \
+  cd $API_PATH && \
+  docker build -t $API_IMG_NAME
 
 # Make the ui image
+UI_PATH=$SOURCE_CODE_PATH/ui
+UI_IMG_NAME=melanoma_ui
+
+cd $UI_PATH && \
+  docker build -t $UI_IMG_NAME
 
 # Download the trained models from registry
+PUBLIC_THESIS_URL=https://gitlab.com/wilberquito/open.thesis.gi
+SAVE_AS=~/open.thesis/
 
-# Wake up the docker compose file
+git lfs clone $PUBLIC_THESIS_URL $SAVE_AS
+
+# Awake docker services using docker compose and variables
+docker compose up
--- Code/README.md
+++ Code/README.md
@@ -3,6 +3,9 @@
 You need docker compose.
 I am using [podman](https://docs.oracle.com/en/learn/podman-compose/index.html#create-a-docker-compose-file).
 
+If you are using `Ubunut`, here are the instructions to install
+[docker](https://docs.docker.com/engine/install/ubuntu/)
+
 ## Run the services
 
 ```bash
