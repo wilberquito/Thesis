edbef962b61a797f4fccc40ed28ec9279ed17b52
wilberquito <wbq.software@gmail.com>
2023-05-03 11:31:22 +0200

    Updates make script

--- Code/Make.sh
+++ Code/Make.sh
@@ -1,6 +1,15 @@
 #!bin/bash
 
-SOURCE_CODE_PATH=/home/wilberquito/repos/Thesis/Code
+WORKING_DIR=$HOME/thesis_deploy
+
+# Clone private (till the momement) this script
+# was created from GitHub
+THESIS_URL=https://gitlab.com/wilberquito/open.thesis.git
+THESIS_SAVE_AS=$WORKING_DIR/thesis/
+
+git clone $THESIS_URL $THESIS_SAVE_AS
+
+SOURCE_CODE_PATH=$WORKING_DIR/thesis/Code
 
 # Make the api image
 VICOROBOT_PKG_PATH=$SOURCE_CODE_PATH/vicorobot.pkg
@@ -13,7 +22,8 @@ cp -rf $VICOROBOT_PKG_PATH $API_PATH && \
   cd $API_PATH && \
   docker build -t $API_IMG_NAME .
 
-if [$? -ne 0]; then
+if [$? -ne 0]
+then
   echo "The construction for the API image did not succeed"
   exit 1
 fi
@@ -32,19 +42,21 @@ fi
 
 # Download the trained models from registry
 PUBLIC_THESIS_URL=https://gitlab.com/wilberquito/open.thesis.git
-SAVE_AS=~/open.thesis/
+PUBLIC_THESIS_SAVE_AS=$WORKING_DIR/open.thesis/
 
-git clone $PUBLIC_THESIS_URL $SAVE_AS
+git clone $PUBLIC_THESIS_URL $PUBLIC_THESIS_SAVE_AS
 
-if [$? -ne 0]; then
+if [$? -ne 0]
+then
   echo "Public assets couldn't be downloaded from registry"
   exit 1
 fi
 
 # Awake docker services using docker compose and variables
-docker compose up
+docker compose up -d
 
-if [$? -ne 0]; then
+if [$? -ne 0]
+then
   echo "Docker services couldn't be awaked"
   exit 1
 fi
