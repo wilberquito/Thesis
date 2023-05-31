39717e889e81dfb295556d9993ce810d5983ad27
wilberquito <wbq.software@gmail.com>
2023-05-31 16:47:17 +0200

    Organizes packages inside the app

--- Code/fastapi/main.py
+++ Code/fastapi/main.py
@@ -8,7 +8,7 @@ def main():
     logging.basicConfig(filename='myapp.log',
                         level=logging.INFO,
                         datefmt='%m/%d/%Y %I:%M:%S %p')
-    uvicorn.run("api:melanoma_api",
+    uvicorn.run("api.api:app",
                 host="0.0.0.0",
                 port=8081,
                 reload=True)
